9e5fa1b5e95b0968cb20dc38988cffa5b7eed3c0
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-11-05 20:34:21 -0800

    update

--- public/write.js
+++ public/write.js
@@ -1,90 +1,3 @@
-// function createNewEntry(word, definition, wordLanguage, definitionLanguage) {
-//     const entryData = {
-//         word: word,
-//         definition: definition,
-//         wordLanguage: wordLanguage,
-//         definitionLanguage: definitionLanguage,
-//     };
-
-//     fetch(`https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition/${word}`, {
-//             method: "GET",
-//             headers: {
-//                 'Accept': 'application/json'
-//             }
-//         }).then((response) => {
-//             if (!response.ok) {
-//                 throw new Error(`HTTP error! Status: ${response.status}`);
-//             }
-//             return Promise.all([response.status, response.json()]); // Include the status code
-//         })
-//         .then(([status, data]) => {
-//             const userResponse = confirm(`The word "${word}" already exists. Do you want to update its definition?`);
-//             if (userResponse) {
-//                 fetch(`https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition/${word}`, {
-//                         method: 'PATCH',
-//                         headers: {
-//                             'Content-Type': 'application/json',
-//                         },
-//                         body: JSON.stringify(entryData),
-//                     })
-//                     .then((response) => {
-//                         if (!response.ok) {
-//                             throw new Error(`HTTP error! Status: ${response.status}`);
-//                         }
-//                         return Promise.all([response.status, response.json()]); // Include the status code
-//                     })
-//                     .then(([status, data]) => {
-//                         displayResponse(status, data); // Pass status and data to the display function
-//                     })
-//                     .catch((error) => {
-//                         console.error('Fetch error:', error);
-//                     });
-//             }
-//         })
-//         .catch((error) => {
-//             fetch('https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition', {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                     },
-//                     body: JSON.stringify(entryData),
-//                 })
-//                 .then((response) => {
-//                     if (!response.ok) {
-//                         throw new Error(`HTTP error! Status: ${response.status}`);
-//                     }
-//                     return Promise.all([response.status, response.json()]); // Include the status code
-//                 })
-//                 .then(([status, data]) => {
-//                     displayResponse(status, data); // Pass status and data to the display function
-//                 })
-//                 .catch((error) => {
-//                     console.error('Fetch error:', error);
-//                 });
-//         });
-// }
-
-// function displayResponse(status, data) {
-//     const responseDisplay = document.getElementById('responseDisplay');
-//     responseDisplay.innerHTML = `
-//       <p>Status Code: ${status}</p>
-//       <pre>${JSON.stringify(data, null, 2)}</pre>`;
-// }
-
-// function handleFormSubmit(event) {
-//     event.preventDefault();
-
-//     const word = document.getElementById('word').value;
-//     const definition = document.getElementById('definition').value;
-//     const wordLanguage = document.getElementById('wordLanguage').value;
-//     const definitionLanguage = document.getElementById('definitionLanguage').value;
-
-//     createNewEntry(word, definition, wordLanguage, definitionLanguage);
-// }
-
-// const form = document.getElementById('dictionaryForm');
-// form.addEventListener('submit', handleFormSubmit);
-
 function createNewEntry(word, definition, wordLanguage, definitionLanguage) {
     const entryData = {
         word: word,
@@ -93,24 +6,61 @@ function createNewEntry(word, definition, wordLanguage, definitionLanguage) {
         definitionLanguage: definitionLanguage,
     };
 
-    fetch('https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition', {
-            method: 'POST',
+    fetch(`https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition/${word}`, {
+            method: "GET",
             headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(entryData),
-        })
-        .then((response) => {
+                'Accept': 'application/json'
+            }
+        }).then((response) => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            return Promise.all([response.status, response.json()]);
+            return Promise.all([response.status, response.json()]); // Include the status code
         })
         .then(([status, data]) => {
-            displayResponse(status, data);
+            const userResponse = confirm(`The word "${word}" already exists. Do you want to update its definition?`);
+            if (userResponse) {
+                fetch(`https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition/${word}`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(entryData),
+                    })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! Status: ${response.status}`);
+                        }
+                        return Promise.all([response.status, response.json()]); // Include the status code
+                    })
+                    .then(([status, data]) => {
+                        displayResponse(status, data); // Pass status and data to the display function
+                    })
+                    .catch((error) => {
+                        console.error('Fetch error:', error);
+                    });
+            }
         })
         .catch((error) => {
-            console.error('Fetch error:', error);
+            fetch('https://serversidelab6-55921ff3f339.herokuapp.com/api/v1/definition', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(entryData),
+                })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                    return Promise.all([response.status, response.json()]); // Include the status code
+                })
+                .then(([status, data]) => {
+                    displayResponse(status, data); // Pass status and data to the display function
+                })
+                .catch((error) => {
+                    console.error('Fetch error:', error);
+                });
         });
 }
 
@@ -134,3 +84,4 @@ function handleFormSubmit(event) {
 
 const form = document.getElementById('dictionaryForm');
 form.addEventListener('submit', handleFormSubmit);
+
